1bb4609bebf8bda327d9fcfdcf6b421b77910ea4
Charles Joshua Uy <uy.charlesjoshua@yahoo.com>
2022-08-01 12:01:11 +0800

    ngl i dunno wat im doing

--- Testing/python/reader.py
+++ Testing/python/reader.py
@@ -1,12 +1,18 @@
 import pandas as pd
 
-#Not gonna lie idk where this file is supposed to be in the directory
-#And i cant figure out how to import models from here
+#### Not gonna lie idk where this file is supposed to be in the directory ###
+# import sys
+# sys.path.append("../../receipts")
+# import models as m
+### Models.py imports not working T~T
 
 xls = pd.ExcelFile("../excel files/Batch 2023.xlsx")
 df_Fees = pd.read_excel(xls, "Fees")
 df_s1 = pd.read_excel(xls, "11 - A")
 df_s2 = pd.read_excel(xls, "11 - B")
 df_s3 = pd.read_excel(xls, "11 - C")
-print(df_s1.head()) 
+
+for index, row in df_s1.iterrows():
+    print(row['Name'])
+
 
---
+++ Testing/python/student.py
@@ -0,0 +1,25 @@
+#Couldn't import it so I modified it just so i can work with the data
+# good luck connecting them hehehe
+
+class Student():
+  def __init__(self, firstname, middlename, lastname, batch, receipts):
+    self.firstname = firstname
+    self.middlename = middlename
+    self.lastname = lastname
+    self.batch = batch
+    self.receipts = receipts
+
+    @property
+    def name(self):
+        return f"{self.firstname} {self.middlename} {self.lastname}"
+
+# class Student():
+#     firstname = db.StringField(max_length=100, required=True)
+#     lastname = db.StringField(max_length=100, required=True)
+#     middlename = db.StringField(max_length=100, required=False, default="")
+#     batch = db.IntField(min_value=2023, max_value=2028)
+#     receipts = db.ListField(db.ReferenceField(Receipt))
+
+#     @property
+#     def name(self):
+#         return f"{self.firstname} {self.middlename} {self.lastname}"
